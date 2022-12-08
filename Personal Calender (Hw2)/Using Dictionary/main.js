01dad279b59f8ed5ca2a58ffc1056698374d6ef4
Jung217 <99934895+Jung217@users.noreply.github.com>
2022-12-08 12:45:54 +0800

    Create main.js

---
+++ Personal Calender (Hw2)/Using Dictionary/main.js
@@ -0,0 +1,53 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import * as render from './render.js'
+
+const posts = [
+  {id:0, title:'Project1', time: "2022-09-16", body:'Home & Login pages'},
+  {id:1, title:'Project2', time: "2022-09-23", body:'Personal Calendar'},
+  {id:2, title:'Midterm Project', time: "2022-09-30", body:'Personal Calendar'}
+];
+
+const router = new Router();
+const app = new Application();
+
+router.get('/', list);
+router.get('/post/new', add);
+router.get('/post/:id', show);
+router.post('/post', create);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+async function list(ctx) {
+  ctx.response.body = await render.list(posts);
+}
+
+async function add(ctx) {
+  ctx.response.body = await render.newPost();
+}
+
+async function show(ctx) {
+  const id = ctx.params.id;
+  const post = posts[id];
+  if (!post) ctx.throw(404, 'invalid post id');
+  ctx.response.body = await render.show(post);
+}
+
+async function create(ctx) {
+  const body = ctx.request.body()
+  if (body.type === "form") {
+    const pairs = await body.value
+    const post = {}
+    for (const [key, value] of pairs) {
+      post[key] = value
+    }
+    console.log('post=', post)
+    const id = posts.push(post) - 1;
+    post.created_at = new Date();
+    post.id = id;
+    ctx.response.redirect('/');
+  }
+}
+
+console.log('Server run at http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
